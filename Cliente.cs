a6dbf349f60ca268751d726633af6e6b1189447a
yMoon-exe <gabriellmarcondes2005@gmail.com>
2025-06-07 08:53:22 -0300

    Criando e validando a classe de Clientes

---
+++ Cliente.cs
@@ -0,0 +1,18 @@
+public class Cliente : ICliente
+{
+    public int Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Email { get; private set; }
+    public string CPF { get; private set; }
+
+    public Cliente(int id, string nome, string email, string cpf)
+    {
+        if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(cpf))
+            throw new ArgumentException("Dados do cliente inválidos.");
+
+        Id = id;
+        Nome = nome;
+        Email = email;
+        CPF = cpf;
+    }
+}
