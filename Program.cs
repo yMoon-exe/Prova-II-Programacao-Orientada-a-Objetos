297b83eb220c4bcfea1b6e0e4b0aee08ababa64c
yMoon-exe <gabriellmarcondes2005@gmail.com>
2025-06-08 10:26:29 -0300

    Testando e começando o Progam.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Teste");
+﻿var cliente = new Cliente(1, "Ana", "ana@mail.com", "12345678900");
+var produto = new Produto(1, "Smartphone", 2500m, "Eletrônicos");
+var item = new ItemPedido { Produto = produto, Quantidade = 1 };
+
+var factory = new PedidoFactory();
+var pedido = factory.CriarPedido(1, cliente, new List<ItemPedido> { item });
+
+IDescontoStrategy desconto = new DescontoPorCategoriaStrategy();
+Console.WriteLine($"Total: {pedido.ValorTotal:C} | Desconto: {desconto.CalcularDesconto(pedido):C}");
