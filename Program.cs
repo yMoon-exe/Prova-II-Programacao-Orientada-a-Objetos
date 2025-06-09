2a29857da14b20eac5b63c26044ed3b3a8c01912
yMoon-exe <gabriellmarcondes2005@gmail.com>
2025-06-09 07:58:14 -0300

    Melhorando o program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
-﻿var cliente = new Cliente(1, "Ana", "ana@mail.com", "12345678900");
-var produto = new Produto(1, "Smartphone", 2500m, "Eletrônicos");
-var item = new ItemPedido { Produto = produto, Quantidade = 1 };
+﻿using System;
+using System.Collections.Generic;
+using LojaVirtual;
 
-var factory = new PedidoFactory();
-var pedido = factory.CriarPedido(1, cliente, new List<ItemPedido> { item });
+namespace LojaVirtual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var produto = new Produto(1, "Smartphone", 2500m, "Eletrônicos");
+            var cliente = new Cliente(1, "Ana", "ana@mail.com", "12345678900");
 
-IDescontoStrategy desconto = new DescontoPorCategoriaStrategy();
-Console.WriteLine($"Total: {pedido.ValorTotal:C} | Desconto: {desconto.CalcularDesconto(pedido):C}");
+            var item = new ItemPedido { Produto = produto, Quantidade = 1 };
+
+            IPedidoFactory factory = new PedidoFactory();
+            var pedido = factory.CriarPedido(1, cliente, new List<ItemPedido> { item });
+
+            IDescontoStrategy desconto = new DescontoPorCategoriaStrategy();
+
+            Console.WriteLine($"Total: {pedido.ValorTotal:C} | Desconto: {desconto.CalcularDesconto(pedido):C}");
+        }
+    }
+}
